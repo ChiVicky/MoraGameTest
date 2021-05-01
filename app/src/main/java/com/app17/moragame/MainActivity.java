cce32b24519af128f3f2dd1a66b6922b6d70ecf7
Vicky <t102360048@ntut.org.tw>
2021-05-01 14:52:47 +0800

    GameOver commit

--- app/src/main/java/com/app17/moragame/MainActivity.java
+++ app/src/main/java/com/app17/moragame/MainActivity.java
@@ -22,28 +22,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Computer.OnComputerCompletedListener {
     //logt
     private static final String TAG = "MainActivity";
-
     public static final int EVEN = 0;
     public static final int PLAYER_WIN = 1;
     public static final int COMPUTER_WIN = 2;
-
     private ImageButton scissorsBtn, rockBtn, paperBtn;
     private Button startBtn, quitBtn;
-    private TextView countText;
-
+    private TextView countText, winCountText, roundText, hartText;
     private ImageView computerImage, playerImage;
-
     private Player player;
     private Computer computer;
-
     private boolean isPlayerRound;
-
     private Timer timer;
-
-    private long gameRoundSeconds = 1000;
+    private long gameRoundSeconds = 1500;
     private int gameRound;
     private boolean isWin;
     private int score;
+    private boolean gameOver = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +46,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViews();
 
-        player = new Player();
         playerImage.setVisibility(View.INVISIBLE);
 
         computer = new Computer();
@@ -74,8 +67,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         timer.start();
     }
 
-    public void initGame() {
+    private void findViews() {
+        scissorsBtn = findViewById(R.id.scissors_ibn);
+        rockBtn = findViewById(R.id.rock_ibn);
+        paperBtn = findViewById(R.id.paper_ibn);
+        startBtn = findViewById(R.id.start_btn);
+        quitBtn = findViewById(R.id.quit_btn);
+        countText = findViewById(R.id.counter_text);
 
+        computerImage = findViewById(R.id.computer_img);
+        playerImage = findViewById(R.id.player_img);
+        winCountText = findViewById(R.id.win_count_text);
+        roundText = findViewById(R.id.round_text);
+        hartText = findViewById(R.id.hart_text);
+
+        View[] views = {scissorsBtn, rockBtn, paperBtn, startBtn, quitBtn};
+        for (View v : views) {
+            v.setOnClickListener(this);
+        }
+    }
+
+    public void initGame() {
+        player = new Player();
+        gameRound = 0;
+        gameRoundSeconds = 1500;
+        isWin = false;
+        score = 0;
+        gameOver = false;
+        roundText.setText(getResources().getString(R.string.round) + " " + (gameRound + 1));
+        winCountText.setText(String.format("%03d", score));
+        String hart = " ";
+        for (int i = 0; i < player.getLife(); i++) {
+            hart += "♥";
+        }
+        hartText.setText(hart);
         if (timer != null) {
             timer.setAlive(false);
         }
@@ -108,29 +133,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Message message = new Message();
         message.what = what;
-        message.obj = String.format("%d:%3d", seconds, milliseconds);
+        message.obj = String.format("%d:%03d", seconds, milliseconds);
         handler.sendMessage(message);
     }
-
-
-    private void findViews() {
-        scissorsBtn = findViewById(R.id.scissors_ibn);
-        rockBtn = findViewById(R.id.rock_ibn);
-        paperBtn = findViewById(R.id.paper_ibn);
-        startBtn = findViewById(R.id.start_btn);
-        quitBtn = findViewById(R.id.quit_btn);
-        countText = findViewById(R.id.count_text);
-
-        computerImage = findViewById(R.id.computer_img);
-        playerImage = findViewById(R.id.player_img);
-
-
-        View[] views = {scissorsBtn, rockBtn, paperBtn, startBtn, quitBtn};
-        for (View v : views) {
-            v.setOnClickListener(this);
-        }
-    }
-
 
     @Override
     public void onClick(View v) {
@@ -169,17 +174,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d(TAG, "onClick: " + getResources().getString(R.string.paper));
                 break;
             case R.id.start_btn:
-                initGame();
+                if (gameOver) {
+                    initGame();
+                }
                 //Toast.makeText(this, getResources().getString(R.string.start), Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "onClick: " + getResources().getString(R.string.start));
                 break;
             case R.id.quit_btn:
+                if ((timer != null) || gameOver) {
+                    timer.setAlive(false);
+                    gameOver = true;
+                }
+//                this.finish();
+//                System.exit(0);
                 //Toast.makeText(this, getResources().getString(R.string.quit), Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "onClick: " + getResources().getString(R.string.quit));
                 break;
         }
-
-
     }
 
     @Override
@@ -223,9 +234,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (state == EVEN) {
             Log.d(TAG, "checkGameState: " + "平手!");
         } else if (state == PLAYER_WIN) {
+            isWin = true;
+            score += 1;
+            winCountText.setText(String.format("%03d", score));
             Log.d(TAG, "checkGameState: " + "玩家勝!");
         } else if (state == COMPUTER_WIN) {
+            player.setLife(player.getLife() - 1);
+            String hart = " ";
+            for (int i = 0; i < player.getLife(); i++) {
+                hart += "♥";
+            }
+            hartText.setText(hart);
+            Log.d(TAG, "hart: " + hart);
             Log.d(TAG, "checkGameState: " + "電腦勝!");
+            if (player.getLife() == 0) {
+                gameOver = true;
+                return;
+            }
         }
 
 
@@ -243,20 +268,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }).start();
     }
 
-    public void nextRound() {
-        if (isWin) {
-            gameRoundSeconds = gameRoundSeconds < 300 ? 300 : gameRoundSeconds - (gameRound / 10) * 100;
-            timer.setTargetMilliseconds(gameRoundSeconds);
-        }
-        gameRound += 1;
-        isPlayerRound = false;
-        playerImage.setVisibility(View.INVISIBLE);
-        isWin = false;
-        timer.reset();
-        Log.d(TAG, " gameRoundSeconds: " + gameRoundSeconds);
-
-        computer.ai();
-    }
 
     private Handler handler = new Handler(Looper.getMainLooper()) {
         @Override
@@ -285,8 +296,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     countText.setText((String) msg.obj);
                     checkGameState();
                     break;
-
+                case 5:
+                    roundText.setText(getResources().getString(R.string.round) + " " + (gameRound + 1));
+                    break;
             }
         }
     };
+
+    public void nextRound() {
+        if (isWin) {
+            gameRoundSeconds -= (gameRound / 10) * 100;
+            if (gameRoundSeconds < 1000) {
+                gameRoundSeconds = 1000;
+            }
+        }
+        timer.setTargetMilliseconds(gameRoundSeconds);
+//        if (isWin) {
+//            gameRoundSeconds = gameRoundSeconds < 300 ? 300 : gameRoundSeconds - (gameRound / 10) * 100;
+//            timer.setTargetMilliseconds(gameRoundSeconds);
+//        }
+        gameRound += 1;
+        handler.sendEmptyMessage(5);
+        isPlayerRound = false;
+        playerImage.setVisibility(View.INVISIBLE);
+        isWin = false;
+        timer.reset();
+        Log.d(TAG, " gameRoundSeconds: " + gameRoundSeconds);
+
+        computer.ai();
+    }
 }
